ad59d9fb907b484d496acc7f38aac7732e0b729e
Aldin <if22b130@technikum-wien.at>
2024-10-02 15:40:29 +0200

    new

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,99 @@
-#include <stdio.h>
 #include <iostream>
+#include <cstdlib>
+#include <dirent.h>
+#include <cerrno>
+#include <cstring>
+#include <cctype>
+#include <unistd.h>
+#include <sys/types.h>
+#include <memory>
+#include <vector>
+
 #include "functions.h"
+
+using namespace std;
+
+void printFile(pid_t pID, const string &fileName, const string &absPath) {
+    cout << pID << ": " << fileName << ": " << absPath << endl;
+}
+
+string getAbsPath(const string &path) {
+    char *realPath = realpath(path.c_str(), nullptr);
+    string result = realPath ? string(realPath) : string();
+    free(realPath); // Remember to free the memory allocated by realpath
+    return result;
+}
+
+bool cmpFileNames(const string &str1, const string &str2, bool caseIns) {
+    if (str1.size() != str2.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < str1.size(); ++i) {
+        char c1 = caseIns ? tolower(str1[i]) : str1[i];
+        char c2 = caseIns ? tolower(str2[i]) : str2[i];
+
+        if (c1 != c2) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+string buildNewPath(const string &oldPath, const string &fileName) {
+    return oldPath + "/" + fileName;
+}
+
+void searchFile(const string &dir, const string &toSearch, bool rec, bool caseIns) {
+    DIR *directory;
+    struct dirent *entry;
+
+    if (!(directory = opendir(dir.c_str()))) {
+        perror(dir.c_str());
+        return;
+    }
+
+    while ((entry = readdir(directory)) != nullptr) {
+        string fileName = entry->d_name;
+
+        switch (entry->d_type) {
+            case DT_DIR: {
+                if (rec) {
+                    if (fileName != "." && fileName != "..") {
+                        string newPath = buildNewPath(dir, fileName);
+                        searchFile(newPath, toSearch, rec, caseIns);
+                    }
+                }
+                break;
+            }
+            case DT_REG: {
+                if (cmpFileNames(fileName, toSearch, caseIns)) {
+                    string combinedName = buildNewPath(dir, fileName);
+                    string absPath = getAbsPath(combinedName);
+                    printFile(getpid(), toSearch, absPath);
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    while ((closedir(directory) == -1 && errno == EINTR));
+}
+
+pid_t forkSearch(const string &dir, const string &toSearch, bool rec, bool caseIns) {
+    pid_t pID = fork();
+
+    if (pID == 0) {
+        searchFile(dir, toSearch, rec, caseIns);
+        exit(EXIT_SUCCESS);
+    } else if (pID == -1) {
+        cerr << "Unable to start child" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return pID;
+}
+
---
+++ functions.h
@@ -0,0 +1,19 @@
+using namespace std;
+
+// Gibt die gefundene Datei aus.
+void printFile(pid_t pID, const string &fileName, const string &absPath);
+
+// Gibt den absoluten Pfad einer Datei aus.
+string getAbsPath(const string &path);
+
+// Zwei Strings werden verglichen abhaengig von caseIns.
+bool cmpFileNames(const string &str1, const string &str2, bool caseIns);
+
+// oldPath und fileName werden kombiniert.
+string buildNewPath(const string &oldPath, const string &fileName);
+
+// Es wird nach einer Datei in dir gesucht. Wenn rec zugewiesen ist, dann recursiv bei den unteren Ordnern auch suchen.
+void searchFile(const string &dir, const string &toSearch, bool rec, bool caseIns);
+
+// In einem neuen Prozess einer Datei suchen.
+pid_t forkSearch(const string &dir, const string &toSearch, bool rec, bool caseIns);
